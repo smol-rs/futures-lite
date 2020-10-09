8fe5d03dff4143018d0a3b774a9fe6df66dd760a
Stjepan Glavina <stjepang@gmail.com>
2020-10-09 13:24:05 +0200

    Fix feature gating

--- src/prelude.rs
+++ src/prelude.rs
@@ -10,9 +10,14 @@
 #[doc(no_inline)]
 pub use crate::{
     future::{Future, FutureExt as _},
+    stream::{Stream, StreamExt as _},
+};
+
+#[cfg(feature = "std")]
+#[doc(no_inline)]
+pub use crate::{
     io::{AsyncBufRead, AsyncBufReadExt as _},
     io::{AsyncRead, AsyncReadExt as _},
     io::{AsyncSeek, AsyncSeekExt as _},
     io::{AsyncWrite, AsyncWriteExt as _},
-    stream::{Stream, StreamExt as _},
 };
