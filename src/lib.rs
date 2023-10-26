50316341f452ce4cbf636f43c493af852825f65f
John Nunley <dev@notgull.net>
2023-10-25 17:50:35 -0700

    Make futures-lite v1 use futures-lite v2 under the hood
    In order to reduce the amount of semver breakage involved with migrating from
    futures-lite v1 to futures-lite v2, this commit makes it so futures-lite v1 uses
    futures-lite v2 under the hood. This way, the vast majority of migrations from
    futures-lite v1 to futures-lite v2 will involve no semver-breakage whatsoever.
    
    Only the future.rs module had any breaking changes, so the rest of the modules
    are just simply imported from futures-lite v2. future.rs itself mostly just
    exposes combinators from v2, except for ready, pending and try_zip, which all
    had semver-incompatible changes.
    
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/lib.rs
+++ src/lib.rs
@@ -21,6 +21,7 @@
 //! }
 //! ```
 
+#![forbid(unsafe_code)]
 #![warn(missing_docs, missing_debug_implementations, rust_2018_idioms)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::needless_borrow)] // suggest code that doesn't work on MSRV
@@ -38,93 +39,10 @@ pub use crate::{
 };
 
 pub mod future;
-pub mod prelude;
-pub mod stream;
+
+#[doc(inline)]
+pub use v2::{pin, prelude, ready, stream};
 
 #[cfg(feature = "std")]
-pub mod io;
-
-/// Unwraps `Poll<T>` or returns [`Pending`][`core::task::Poll::Pending`].
-///
-/// # Examples
-///
-/// ```
-/// use futures_lite::{future, prelude::*, ready};
-/// use std::pin::Pin;
-/// use std::task::{Context, Poll};
-///
-/// fn do_poll(cx: &mut Context<'_>) -> Poll<()> {
-///     let mut fut = future::ready(42);
-///     let fut = Pin::new(&mut fut);
-///
-///     let num = ready!(fut.poll(cx));
-///     # drop(num);
-///     // ... use num
-///
-///     Poll::Ready(())
-/// }
-/// ```
-///
-/// The `ready!` call expands to:
-///
-/// ```
-/// # use futures_lite::{future, prelude::*, ready};
-/// # use std::pin::Pin;
-/// # use std::task::{Context, Poll};
-/// #
-/// # fn do_poll(cx: &mut Context<'_>) -> Poll<()> {
-///     # let mut fut = future::ready(42);
-///     # let fut = Pin::new(&mut fut);
-///     #
-/// let num = match fut.poll(cx) {
-///     Poll::Ready(t) => t,
-///     Poll::Pending => return Poll::Pending,
-/// };
-///     # drop(num);
-///     # // ... use num
-///     #
-///     # Poll::Ready(())
-/// # }
-/// ```
-#[macro_export]
-macro_rules! ready {
-    ($e:expr $(,)?) => {
-        match $e {
-            core::task::Poll::Ready(t) => t,
-            core::task::Poll::Pending => return core::task::Poll::Pending,
-        }
-    };
-}
-
-/// Pins a variable of type `T` on the stack and rebinds it as `Pin<&mut T>`.
-///
-/// ```
-/// use futures_lite::{future, pin};
-/// use std::fmt::Debug;
-/// use std::future::Future;
-/// use std::pin::Pin;
-/// use std::time::Instant;
-///
-/// // Inspects each invocation of `Future::poll()`.
-/// async fn inspect<T: Debug>(f: impl Future<Output = T>) -> T {
-///     pin!(f);
-///     future::poll_fn(|cx| dbg!(f.as_mut().poll(cx))).await
-/// }
-///
-/// # spin_on::spin_on(async {
-/// let f = async { 1 + 2 };
-/// inspect(f).await;
-/// # })
-/// ```
-#[macro_export]
-macro_rules! pin {
-    ($($x:ident),* $(,)?) => {
-        $(
-            let mut $x = $x;
-            #[allow(unused_mut)]
-            let mut $x = unsafe {
-                core::pin::Pin::new_unchecked(&mut $x)
-            };
-        )*
-    }
-}
+#[doc(inline)]
+pub use v2::io;
